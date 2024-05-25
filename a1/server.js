fb78bc7e692a49bededf04b1e3048ec1efac93ed
tahmedqq <zyan09746@gmail.com>
2024-05-25 13:01:40 -0400

    assignment 1

---
+++ a1/server.js
@@ -0,0 +1,16 @@
+/**
+*  WEB322 - Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy. 
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites, friends gpt or otherwise) or distributed to other students.
+*  I understand that if caught doing so, I will receive zero on this assignment and possibly 
+*  fail the entire course.
+*  Name: Tareq Ahmed
+*  Student ID: 123067233
+*  Date:25/05/2024
+*  Vercel Web App URL: https://vercel.com/tareq-ahmeds-projects-e59fdeb1
+Team Code - tareq-ahmeds-projects-e59fdeb1
+*  GitHub Repository URL: https://github.com/tahmed99/helloworld
+**/
+
+console.log("Tareq Ahmed",123067233);
