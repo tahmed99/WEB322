93775af92c0f3ddfac78b443fc6b3908883a191f
tahmedqq <zyan09746@gmail.com>
2024-07-11 11:03:50 -0400

    Test 4

---
+++ Test 4/data.js
@@ -0,0 +1,20 @@
+const data = {
+    users: [
+      { id: 1, name: "Tareq", email: "tareq@yahoo.com" },
+      { id: 2, name: "Rohan", email: "rohan@gmail.com" },
+      { id: 3, name: "Zyana", email: "zyana@gmail.com" },
+      { id: 4, name: "Zenith", email: "zenith@yahoo.com"},
+      { id: 5, name: "Meher", email: "meher@yahoo.com"},
+      { id: 6, name: "Ali", email: "ali@gmail.com"}
+    ],
+    products: [
+      { PID: 1, name: "Laptop", price: 1200 },
+      { PID: 2, name: "Smartphone", price: 800 },
+      { PID: 3, name: "Tablet", price: 500 },
+      { PID: 4, name: "Mouse", price: 250 },
+      { PID: 5, name: "Keyboard", price: 300 },
+      { PID: 6, name: "Personal Computer", price: 5000 }
+    ]
+  };
+  
+  module.exports = data;
